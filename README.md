8572b380d30da1d8c7a6bbbf9690d64a1576ca57
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-08-19 00:49:52 -0500

    finished product

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 ## Description
 
-I was tasked with adding JavaScript code to make this password generator functionable.
+I was tasked with creating a functioning password generator using JavaScript. I used arrays and if functions to let the browser collect user input via prompts and generate a password that is randomized and pertaining to the correct input. I added a countdown timer that counts down until the generated password is deleted so that nobody else can see the password that was generated.
 
 ## Installation
 
-I used arrays and if functions to let the browser collect user input via prompts and generate a password that is randomized and pertaining to the correct input.
+No installation necessary but you can clone the [repository](https://github.com/MrMessyFace/password-generator) from GitHub to edit it and make your own version.
 
 ## Usage
 
-To use this page, you click "Generate Password" and it will ask you series of questions. You will then select how many characters you would like, then if you would like to include special characters, then numeric characters, then lowercase letters, and then uppercase characters. Then a randomized password will generate with the exact perameters you have selected. OK means yes and cancel means no. If you do not select a character limit, it informs you that you need to and restarts. If you choose a limit that is not between 8 and 128, it will inform you to do it again. If you choose a limit but no choices, it will inform you to do so and clear the field. Once you have successfully generated a random password, a countdown appears showing that the new password will delete after 10 seconds.
+To use this page, you click "Generate Password" and it will ask you series of questions. You will select how many characters you would like, if you would like to include special characters, numeric characters, lowercase letters, and/or uppercase characters. Then a randomized password will generate with the exact perameters you have selected. OK means yes and cancel means no. If you do not select a character limit, it informs you that you need to and restarts. If you choose a limit that is not between 8 and 128, it will inform you to do it again. If you choose a limit but no choices, it will inform you to do so and clear the field. Once you have successfully generated a random password, a countdown appears showing that the new password will delete after 10 seconds.
 
-![Screenshot](assets/images/mrmessyface.github.io_03-password-generator_.png)
+![Screenshot](assets/images/password-generator-screenshot.jpg)
 
 ## Credits
 
-HTML, CSS, and starter JavaScript code were provided by my bootcamp instructors and most of the JavaScript code was written from scratch through trial and error, some looking back at activities done in class, and searching via Google.
+HTML, CSS, and starter JavaScript code were provided by the Northwestern University Full Stack Coding Bootcamp and most of the JavaScript code was written from scratch, referencing class curriculum.
 
-https://mrmessyface.github.io/password-generator/
+## Helpful Links
+
+- [Repository](https://github.com/MrMessyFace/password-generator)
+- [Live App](https://mrmessyface.github.io/password-generator/)
+
+&copy; 2023 Jessie S. Galindo. All Rights Reserved.
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,55 +7,157 @@ var confirmNumeric;
 var confirmLowercase;
 var confirmUppercase;
 
-special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+special = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 function generatePassword() {
   // Ask the user to choose a character limit for the password.
-  var enterLimit = parseFloat(prompt("How many characters would you like your password to contain?"));
+  var enterLimit = parseFloat(
+    prompt("How many characters would you like your password to contain?")
+  );
   // If they first press cancel.
   if (!enterLimit) {
     alert("This needs a value!");
-    var enterLimit = parseFloat(prompt("How many characters would you like your password to contain?"));
-  };
+    var enterLimit = parseFloat(
+      prompt("How many characters would you like your password to contain?")
+    );
+  }
 
   // If they choose outside the limit.
   if (enterLimit < 8 || enterLimit > 128) {
     var enterLimit = parseFloat(prompt("You must choose between 8 and 128."));
-  };
+  }
   // If they choose inside the limit.
   if (enterLimit < 8 || enterLimit > 128) {
     var enterLimit;
   } else {
-    var confirmSpecial = confirm("Click OK to confirm including special characters.")
-    var confirmNumeric = confirm("Click OK to confirm including numeric characters.");
-    var confirmLowercase = confirm("Click OK to confirm including lowercase characters.");
-    var confirmUppercase = confirm("Click OK to confirm including uppercase characters.");
-  };
+    var confirmSpecial = confirm(
+      "Click OK to confirm including special characters."
+    );
+    var confirmNumeric = confirm(
+      "Click OK to confirm including numeric characters."
+    );
+    var confirmLowercase = confirm(
+      "Click OK to confirm including lowercase characters."
+    );
+    var confirmUppercase = confirm(
+      "Click OK to confirm including uppercase characters."
+    );
+  }
 
   // Choices accesses the arrays.
   var choices = [];
 
   // If Special is chosen.
   if (confirmSpecial) {
-    choices = choices.concat(special)
+    choices = choices.concat(special);
   }
   // If Numeric is chosen.
   if (confirmNumeric) {
-    choices = choices.concat(numeric)
+    choices = choices.concat(numeric);
   }
   // If Lowercase is chosen.
   if (confirmLowercase) {
-    choices = choices.concat(lowercase)
+    choices = choices.concat(lowercase);
   }
   // If Uppercase is chosen.
   if (confirmUppercase) {
-    choices = choices.concat(uppercase)
+    choices = choices.concat(uppercase);
   }
   // If none of the choices are chosen.
-  if (!confirmSpecial && !confirmNumeric && !confirmLowercase && !confirmUppercase) {
+  if (
+    !confirmSpecial &&
+    !confirmNumeric &&
+    !confirmLowercase &&
+    !confirmUppercase
+  ) {
     alert("You must choose at least one parameter.");
     return "";
   }
@@ -63,7 +165,8 @@ function generatePassword() {
   var randomPassword = "";
 
   for (var i = 0; i < enterLimit; i++) {
-    randomPassword = randomPassword + choices[Math.floor(Math.random() * choices.length)];
+    randomPassword =
+      randomPassword + choices[Math.floor(Math.random() * choices.length)];
   }
   return randomPassword;
 }
@@ -73,11 +176,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  if (passwordText.value = password) {
+  if ((passwordText.value = password)) {
     setTime();
-  };
+  }
   if (randomPassword === "") {
-
   }
 }
 
@@ -91,7 +193,9 @@ var secondsLeft = 11;
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
-    timerEl.textContent = secondsLeft + " seconds until your randomly generated password is deleted.";
+    timerEl.textContent =
+      secondsLeft +
+      " seconds until your randomly generated password is deleted.";
     if (secondsLeft === 0) {
       location.reload();
     }
